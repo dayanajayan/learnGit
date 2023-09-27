098bd8ae513d5cd6c17024f3d1722d9866fb6c20
Dayana Jayan <dayana18jayan@gmail.com>
2023-09-27 18:57:27 +0530

    voting system of students if eligible or not and no of students eligible

---
+++ voting.js
@@ -0,0 +1,40 @@
+let studentsDetails=[{
+    name:"Jine",
+    age:23,
+    gender:"female"
+},
+{
+    name:"Jinu",
+    age:13,
+    gender:"male"
+},
+{
+    name:"Jayson",
+    age:32,
+    gender:"male"
+},
+{
+    name:"Sahana",
+    age:12,
+    gender:"female"
+},
+{
+    name:"Shruthi",
+    age:22,
+    gender:"female"
+}]
+let count=0;
+
+for (let i=0; i< studentsDetails.length; i++)
+
+if (studentsDetails[i].age<18)
+{
+    console.log(studentsDetails[i].name +" "+ studentsDetails[i].gender+" is not eligible to vote");
+
+}
+else
+{
+    console.log(studentsDetails[i].name +" "+ studentsDetails[i].gender+" is eligible to vote")
+    count++;
+}
+console.log("No of student eligible to vote : "+ count);
